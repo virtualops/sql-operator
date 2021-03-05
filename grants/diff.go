f57da064a6ffb50c5469751ff8b25dc6b59d51f1
Leo Sjöberg <leo.sjoberg@gmail.com>
2021-03-05 22:51:59 +0200

    Support diffing grants on the same target

--- grants/diff.go
+++ grants/diff.go
@@ -1,7 +1,6 @@
 package grants
 
 import (
-	"github.com/juliangruber/go-intersect"
 	"github.com/virtualops/sql-operator/api/v1alpha1"
 	"reflect"
 )
@@ -15,17 +14,17 @@ type GrantDiff struct {
 // containing [current - intersection] [intersection] [new - intersection]
 func SegmentByTarget(currentGrants, newGrants []v1alpha1.GrantSpec) ([]v1alpha1.GrantSpec, [][2]v1alpha1.GrantSpec, []v1alpha1.GrantSpec) {
 	// we create a map from the hash identifier (grant target) to a slice of
-	// two indices – the index in current, and the index in new
+	// two indices – the index in remove, and the index in new
 	hashMap := map[string]int{}
 
 	// because we're modifying slices directly, we need to make local copies to not modify the source slices
-	current := make([]v1alpha1.GrantSpec, len(currentGrants))
+	remove := make([]v1alpha1.GrantSpec, len(currentGrants))
 	new := make([]v1alpha1.GrantSpec, len(newGrants))
 
-	copy(current, currentGrants)
+	copy(remove, currentGrants)
 	copy(new, newGrants)
 
-	for i, spec := range current {
+	for i, spec := range remove {
 		hashMap[spec.Target] = i
 	}
 
@@ -66,25 +65,25 @@ func SegmentByTarget(currentGrants, newGrants []v1alpha1.GrantSpec) ([]v1alpha1.
 	}
 
 	for _, i := range hashMap {
-		record := current[i]
+		record := remove[i]
 		records := intersection[record.Target]
 		records[0] = record
 		intersection[record.Target] = records
 
 		// set a zero value for all to remove
-		current[i] = v1alpha1.GrantSpec{}
+		remove[i] = v1alpha1.GrantSpec{}
 	}
 
 	i = 0
 	for {
-		if len(current) <= i {
+		if len(remove) <= i {
 			break
 		}
 		// if we have a zero object, we unshift it and keep the index as is
-		// since the next element will now have the current index.
-		if reflect.ValueOf(current[i]).IsZero() {
-			copy(current[i:], current[i+1:])
-			current = current[:len(current)-1]
+		// since the next element will now have the remove index.
+		if reflect.ValueOf(remove[i]).IsZero() {
+			copy(remove[i:], remove[i+1:])
+			remove = remove[:len(remove)-1]
 			continue
 		}
 
@@ -92,20 +91,64 @@ func SegmentByTarget(currentGrants, newGrants []v1alpha1.GrantSpec) ([]v1alpha1.
 		i++
 	}
 
-	//copy(current[i:], current[i+1:])               // Shift a[i+1:] left one index.
-	//current[len(current)-1] = v1alpha1.GrantSpec{} // Erase last element (write zero value).
-	//current = current[:len(current)-1]             // Truncate slice.
+	//copy(remove[i:], remove[i+1:])               // Shift a[i+1:] left one index.
+	//remove[len(remove)-1] = v1alpha1.GrantSpec{} // Erase last element (write zero value).
+	//remove = remove[:len(remove)-1]             // Truncate slice.
 
 	var output [][2]v1alpha1.GrantSpec
 	for _, set := range intersection {
 		output = append(output, set)
 	}
 
-	return current, output, new
+	return remove, output, new
 }
 
-func DiffPrivileges(curr, new []string) GrantDiff {
-	return GrantDiff{}
+func DiffPrivileges(curr, new v1alpha1.GrantSpec) GrantDiff {
+	diff := GrantDiff{}
+
+	if new.Privileges[0] == "*" {
+		diff.Grant = append(diff.Grant, new)
+
+		return diff
+	}
+
+	diff.Revoke  = []v1alpha1.GrantSpec{{
+		Target: curr.Target,
+	}}
+
+	for _, privilege := range curr.Privileges {
+		revoke := true
+		for _, retainedPrivilege := range new.Privileges {
+			if privilege == retainedPrivilege {
+				revoke = false
+				break
+			}
+		}
+
+		if revoke {
+			diff.Revoke[0].Privileges = append(diff.Revoke[0].Privileges, privilege)
+		}
+	}
+
+	diff.Grant = []v1alpha1.GrantSpec{{
+		Target: curr.Target,
+	}}
+
+	for _, privilege := range new.Privileges {
+		grant := true
+		for _, oldPrivilege := range curr.Privileges {
+			if privilege == oldPrivilege {
+				grant = false
+				break
+			}
+		}
+
+		if grant {
+			diff.Grant[0].Privileges = append(diff.Grant[0].Privileges, privilege)
+		}
+	}
+
+	return diff
 }
 
 func GenerateExecutionPlan(current, new []v1alpha1.GrantSpec) GrantDiff {
@@ -119,54 +162,16 @@ func GenerateExecutionPlan(current, new []v1alpha1.GrantSpec) GrantDiff {
 	diff.Grant = add
 	// 4. Loop through the target intersection and generate a permissions diff per target
 	for _, intersection := range update {
-		// here we want to generate a REVOKE/GRANT list
-		oldPerms := intersection[0]
-		newPerms := intersection[1]
+		innerDiff := DiffPrivileges(intersection[0], intersection[1])
 
-		// if the new privileges are all privileges, we can do an early return
-		if newPerms.Privileges[0] == "*" {
-			diff.Grant = append(diff.Grant, newPerms)
-			continue
+		if len(innerDiff.Grant[0].Privileges) > 0 {
+			diff.Grant = append(diff.Grant, innerDiff.Grant[0])
 		}
 
-		var permsIntersect []string
-
-		for _, t := range intersect.Hash(oldPerms.Privileges, newPerms.Privileges).([]interface{}) {
-			permsIntersect = append(permsIntersect, t.(string))
+		if len(innerDiff.Revoke[0].Privileges) > 0 {
+			diff.Revoke = append(diff.Revoke, innerDiff.Revoke[0])
 		}
 	}
 
 	return diff
-}
-
-func buildTargetIntersection(current, new []v1alpha1.GrantSpec) []v1alpha1.GrantSpec {
-	// Check for any grants to change
-	// 1. Target intersection
-	//var currTargets []string
-	//var newTargets []string
-	//for _, g := range current {
-	//	currTargets = append(currTargets, g.Target)
-	//}
-	//for _, g := range new {
-	//	newTargets = append(newTargets, g.Target)
-	//}
-	//
-	//var targetIntersect []string
-	//
-	//for _, t := range intersect.Hash(currTargets, newTargets).([]interface{}) {
-	//	targetIntersect = append(targetIntersect, t.(string))
-	//}
-	//
-	//return targetIntersect
-	return nil
-}
-
-func containsString(input []string, search string) bool {
-	for _, i := range input {
-		if i == search {
-			return true
-		}
-	}
-
-	return false
 }
--- grants/diff_test.go
+++ grants/diff_test.go
@@ -37,3 +37,41 @@ func TestSegmentByTarget(t *testing.T) {
 	assert.Equal(t, currentGrants[1], update[0][0])
 	assert.Equal(t, newGrants[0], update[0][1])
 }
+
+func TestGenerateExecutionPlan(t *testing.T) {
+	currentGrants := []v1alpha1.GrantSpec{
+		{
+			Target:     "test.remove",
+			Privileges: []string{"CREATE", "UPDATE", "SELECT"},
+		},
+		{
+			Target:     "test.change",
+			Privileges: []string{"CREATE", "UPDATE", "SELECT"},
+		},
+	}
+	newGrants := []v1alpha1.GrantSpec{
+		{
+			Target:     "test.change",
+			Privileges: []string{"CREATE", "UPDATE", "DELETE"}, // we want -SELECT +DELETE
+		},
+		{
+			Target:     "test.add",
+			Privileges: []string{"*"},
+		},
+	}
+
+	diff := GenerateExecutionPlan(currentGrants, newGrants)
+	assert.Len(t, diff.Grant, 2) // 1 grant to add test.add, one to GRANT DELETE
+	assert.Len(t, diff.Revoke, 2) // 1 revoke to remove test.remove, one to REVOKE SELECT
+
+	assert.Equal(t, newGrants[1], diff.Grant[0])
+	assert.Equal(t, currentGrants[0], diff.Revoke[0])
+	assert.Equal(t, v1alpha1.GrantSpec{
+		Target: "test.change",
+		Privileges: []string{"DELETE"},
+	}, diff.Grant[1])
+	assert.Equal(t, v1alpha1.GrantSpec{
+		Target: "test.change",
+		Privileges: []string{"SELECT"},
+	}, diff.Revoke[1])
+}
